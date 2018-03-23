dea51efbdee4aa01917a91353065db72769ddf9b
vladkub <32514035+vladkub@users.noreply.github.com>
2018-03-23 14:34:26 +0400

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+//требуемые плагины: sass, jade, watch, browser-sync, gulp-autoprefixer
+
 var gulp=require('gulp');
 var sass=require('gulp-sass');
 var jade = require('gulp-jade');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('browserSync', function() { // Создаем таск browser-sync
     browserSync({ // Выполняем browser Sync
@@ -17,6 +20,7 @@ gulp.task('browserSync', function() { // Создаем таск browser-sync
 gulp.task('sass', function() {
 	gulp.src('app/scss/*.sass')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
 		.pipe(gulp.dest('app/css/'))
 		.pipe(browserSync.reload({stream: true}));
 });    
@@ -25,10 +29,12 @@ gulp.task('sass', function() {
 gulp.task('jade', function() {
     return gulp.src('app/jade/*.jade')
         .pipe(jade({pretty: true})) 
-        .pipe(gulp.dest('app/html/')); // указываем gulp куда положить скомпилированные HTML файлы
+        .pipe(gulp.dest('app/html/')) // указываем gulp куда положить скомпилированные HTML файлы
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
+gulp.task('watch', ['browserSync', 'sass', 'jade'], function() {
 	gulp.watch('app/scss/*.sass', ['sass']);
+	gulp.watch('app/jade/*.jade', ['jade']);
 });
 
